909a209774c551300cfe59aa8bc8de140a546562
Yesid <yesidrodriguez305@gmail.com>
2022-09-27 10:03:34 -0500

    Add es6: template literals (template strings)

---
+++ src/es6/02-strings.js
@@ -0,0 +1,19 @@
+// CONCATENATION
+let hello = 'Hello';
+let world = 'World';
+let epicPhrase = hello + ' ' + world + '!';
+console.log(epicPhrase);  
+
+// TEMPLATE LITERALS
+let epicPhrase2 = `${hello} ${world}!`;
+console.log(epicPhrase2);
+
+// MULTI-LINE STRINGS
+let lorem = 'Esto es un string \n' + 'Esto es otra linea';
+
+let lorem2 = `Esto es una frase epica
+La continuación de la frase epica.
+`;
+
+console.log(lorem);
+console.log(lorem2);
