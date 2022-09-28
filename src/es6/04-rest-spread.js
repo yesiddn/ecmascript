aeeff364ce3bb9b5b1f837663b854ad7df27c006
Yesid <yesidrodriguez305@gmail.com>
2022-09-28 11:04:03 -0500

    Add es6: spread operator and rest parameter

--- src/es6/04-rest-spread.js
+++ src/es6/04-rest-spread.js
@@ -7,3 +7,19 @@ console.log(a, b, c);
 let user = { userName: 'Jess', age: 17 };
 let { userName, age } = user;
 console.log(userName, age);
+
+// SPREAD OPERATOR
+let person = { name: 'Jess', age: 17 };
+let country = 'CO';
+
+let data = { id: 1, ...person, country };
+console.log(data);
+
+// REST OPERATOR
+function sum(num1, ...values) {
+  console.log(values);
+  console.log(num1 + values[0]);
+  return values[0];
+}
+
+sum(1, 2, 3, 4, 5);
