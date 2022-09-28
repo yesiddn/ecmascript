30a7bd8f04b136074d7362a2cbc943891a09adc3
Yesid <yesidrodriguez305@gmail.com>
2022-09-28 10:24:49 -0500

    Add es6: asignación de desestructuración

---
+++ src/es6/04-rest-spread.js
@@ -0,0 +1,9 @@
+// ARRAYS DESTRUCTURING
+let fruits = ['apple', 'banana', 'orange'];
+let [a, b, c] = fruits;
+console.log(a, b, c);
+
+// OBJECTS DESTRUCTURING
+let user = { userName: 'Jess', age: 17 };
+let { userName, age } = user;
+console.log(userName, age);
