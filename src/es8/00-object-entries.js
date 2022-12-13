644cd1ca9a59eb7851943fb2b03223438168995f
Yesid <yesidrodriguez305@gmail.com>
2022-12-13 17:31:19 -0500

    Add es8: object entries

---
+++ src/es8/00-object-entries.js
@@ -0,0 +1,4 @@
+// Return an array of arrays with the key and value of an object
+const countries = { MX: 'Mexico', CO: 'Colombia', CL: 'Chile', PE: 'Peru' };
+console.log(Object.entries(countries));
+// Output: [ [ 'MX', 'Mexico' ], [ 'CO', 'Colombia' ], [ 'CL', 'Chile' ], [ 'PE', 'Peru' ] ]
