3c9de086e36f0587db2b5ec3724c62f5fd222da3
Yesid <yesidrodriguez305@gmail.com>
2022-12-13 18:15:33 -0500

    Add es8: trailing commas

---
+++ src/es8/04-trailing-commas.js
@@ -0,0 +1,12 @@
+const array = [23, 25, 19, 17, 32];
+
+console.log(array);
+console.log(array.length);
+// [23, 25, 19, 17, 32] 5
+
+// trailing commas
+const array2 = [23, 25, 19, 17, 32, , , , 45];
+
+console.log(array2);
+console.log(array2.length);
+// [23, 25, 19, 17, 32, empty × 3, 45] 9
