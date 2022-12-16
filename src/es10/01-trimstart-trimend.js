3fc9557eaa7ba7d79a2c93c9558d5abf9e7ae1dd
Yesid <yesidrodriguez305@gmail.com>
2022-12-16 17:53:55 -0500

    Add es10: trimStart and trimEnd

---
+++ src/es10/01-trimstart-trimend.js
@@ -0,0 +1,7 @@
+// trimStart() deletes spaces from the beginning of a string
+const hello = '   hello world   ';
+console.log(hello);
+console.log(hello.trimStart());
+
+// trimEnd() deletes spaces from the end of a string
+console.log(hello.trimEnd());
