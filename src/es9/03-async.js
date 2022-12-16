cf022f98f7bba3eafe0da31002384ea15319a5a7
Yesid <yesidrodriguez305@gmail.com>
2022-12-16 17:05:41 -0500

    Add es9: update async

---
+++ src/es9/03-async.js
@@ -0,0 +1,22 @@
+async function* anotherGenerator() {
+  yield await Promise.resolve(1);
+  yield await Promise.resolve(2);
+  yield await Promise.resolve(3);
+}
+
+const other = anotherGenerator();
+other.next().then((response) => console.log(response.value));
+other.next().then((response) => console.log(response.value));
+console.log('Hello');
+// Hello
+// 1
+// 2
+
+async function arrayOfNames(array) {
+  for await (let name of array) {
+    console.log(name);
+  }
+}
+
+const names = arrayOfNames(['Luis', 'Juan', 'Pedro']);
+console.log('After');
