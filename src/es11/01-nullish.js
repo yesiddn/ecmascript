6ede5150a54aa1060ebb897afffd387c1c68619c
Yesid <yesidrodriguez305@gmail.com>
2023-01-23 11:15:41 -0500

    Add es11: nullish

---
+++ src/es11/01-nullish.js
@@ -0,0 +1,5 @@
+// const anotherNumber = 1;
+const anotherNumber = null;
+const validate = anotherNumber ?? 5; // valida si el valor es null o undefined y en caso de serlo asigna el valor 5
+
+console.log(validate);
