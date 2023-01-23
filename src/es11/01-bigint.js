26e00bdef5323e1af6149602b027c62837640314
Yesid <yesidrodriguez305@gmail.com>
2023-01-23 11:08:57 -0500

    Add es11: bigint

---
+++ src/es11/01-bigint.js
@@ -0,0 +1,5 @@
+const aBigNumber = 1234567890123456789012345678901234567890n; // la n indica que es un bigint
+const anotherBigNumber = BigInt(1234567890123456789012345678901234567890); // con esto no hace falta poner la n
+
+console.log(aBigNumber);
+console.log(anotherBigNumber);
