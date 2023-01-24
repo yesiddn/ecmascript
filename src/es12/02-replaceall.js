a46e4235be2dba2623079969ccbcf709c0b632e2
Yesid <yesidrodriguez305@gmail.com>
2023-01-24 10:46:22 -0500

    Add es12: replaceAll

---
+++ src/es12/02-replaceall.js
@@ -0,0 +1,6 @@
+// Replace all occurrences of a string with another string
+const string = 'JavaScript is an awesome programming language';
+console.log(string);
+
+const replacedString = string.replace('JavaScript', 'TypeScript');
+console.log(replacedString);
