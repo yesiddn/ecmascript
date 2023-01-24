ac1cccc1a7b7f3e4a36e1ccb85011c930afb87ad
Yesid <yesidrodriguez305@gmail.com>
2023-01-24 11:50:08 -0500

    Add es13: array.at()

---
+++ src/es13/00-at.js
@@ -0,0 +1,7 @@
+const array = ['one', 'two', 'three', 'four', 'five'];
+
+// obtener el ultimo elemento de un array antes de ES13
+console.log(array[array.length - 1]);
+
+// obtener el ultimo elemento de un array con ES13
+console.log(array.at(-1));
